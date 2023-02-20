86f958c0dd874232cefaea290ea23faa4682c12c
yosefalan <yosefalan@gmail.com>
2023-02-19 23:28:51 -0600

    setting up server

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import SocketsProvider from '../context/socket.context';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <SocketsProvider><Component {...pageProps} /></SocketsProvider>
 }
